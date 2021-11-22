e20e53e75a9da38f83740141066376a0eb4dbb47
tienpm98 <phaminhtien2498@gmail.com>
2021-11-23 00:43:16 +0700

    run script deploy

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const Dashboard = () => {
-	return <div>dashboard</div>
+	return <div>
+		<h1>Hello React</h1>
+		<p>This project created by tienpm</p>
+	</div>
 }
 
 export default Dashboard
