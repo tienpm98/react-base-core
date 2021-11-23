65c584588991b404647004c8c17f19987ac86117
tienpm98 <phaminhtien2498@gmail.com>
2021-11-23 23:37:29 +0700

    test circleci

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const Dashboard = () => {
-	return <div>
-		<h1>Hello React</h1>
-		<p>This project created by tienpm</p>
-	</div>
+	return (
+		<div>
+			<h1>Hello human</h1>
+			<h2>This is react core</h2>
+			<p>This project created by tienpm. Using:</p>
+			<ul>
+				<li>Webpack</li>
+				<li>Circle ci</li>
+				<li>AWS EC2</li>
+			</ul>
+		</div>
+	)
 }
 
 export default Dashboard
